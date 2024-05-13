027778710b986531a3b76701fe07454dca951689
miyamarinova <miyamarinova@gmail.com>
2024-05-13 16:40:25 -0400

    Static files always loaded

--- petstagram/urls.py
+++ petstagram/urls.py
@@ -14,8 +14,8 @@ urlpatterns = [
 ]
 
 # Show the Uploaded Images
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 send_mail(
     subject="It works",
