19dafb8a35ffcf3eb6888a5467a09f99d3797d8f
miyamarinova <miyamarinova@gmail.com>
2024-07-02 05:27:13 -0400

    edit allowed hosts

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ['petstragram-miyamarinova.onrender.com', '*.onrender.com', 'localhost', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = [f'https://{host}' for host in ALLOWED_HOSTS if host]
 
 if 'localhost' in ALLOWED_HOSTS or '127.0.0.1' in ALLOWED_HOSTS:
