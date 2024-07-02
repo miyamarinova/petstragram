b07ac25810c0c52aef2c9452380d118c5220f0e0
miyamarinova <miyamarinova@gmail.com>
2024-07-02 05:14:19 -0400

    edit allowed_hosts

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -12,10 +12,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+CSRF_TRUSTED_ORIGINS = [f'https://{host}' for host in ALLOWED_HOSTS if host]
 
-ALLOWED_HOSTS = [host.strip() for host in ALLOWED_HOSTS if host.strip()]
-
-CSRF_TRUSTED_ORIGINS = [f'https://{host}' for host in ALLOWED_HOSTS]
+if 'localhost' in ALLOWED_HOSTS or '127.0.0.1' in ALLOWED_HOSTS:
+    CSRF_TRUSTED_ORIGINS += ['http://localhost', 'http://127.0.0.1']
 
 INSTALLED_APPS = [
     # Django apps
